5e0215d3bf87e2e87d5fe48aa5ae6b9766f61a9b
z051-pixel <zinmonthet051@gmail.com>
2025-12-15 06:04:04 +0630

    Update check_streamlit_version.py

--- check_streamlit_version.py
+++ check_streamlit_version.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import numpy
+import numpy as np
 import pandas as pd
 import sklearn
 
-st.write("NumPy:", numpy.__version__)
-st.write("Pandas:", pandas.__version__)
+st.write("NumPy:", np.__version__)
+st.write("Pandas:", pd.__version__)
 st.write("Scikit-learn:", sklearn.__version__)
